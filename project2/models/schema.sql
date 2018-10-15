37c4cdf05e9a3aa439acc0d65bba7b78e93a6ff3
Jd0h Norteman <jnorteman91@gmail.com>
2018-10-15 13:02:42 -0400

    added schema.sql, seeds.sql and set up the server

--- project2/models/schema.sql
+++ project2/models/schema.sql
@@ -1,5 +1,12 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS volunteer_db;
+CREATE DATABASE volunteer_db;
+USE volunteer_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE volunteers (
+    id INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(45) NOT NULL,
+    Location VARCHAR(200) NOT NULL,
+    Available VARCHAR(155) NOT NULL,
+    Group_Size INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
