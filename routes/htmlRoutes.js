c7e0dfcae4e9a3e20a3182afa4a5858d376c971f
Jd0h Norteman <jnorteman91@gmail.com>
2018-10-17 18:37:28 -0400

    html routes added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,24 @@
-var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load contact page
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/contact.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // Load volunteer page 
+  app.get("/volunteer", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+  }); 
+
+  // Load login page
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/authTest.html"));
   });
 };
+
