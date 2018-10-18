d6c54a0f906a6dc101f3b6268ef11fb6b5909e74
james inch <james.inch0103@gmail.com>
2018-10-17 21:01:49 -0400

    adjusted file paths

---
+++ routes/htmlRoutes.1.js
@@ -0,0 +1,23 @@
+var path = require("path");
+
+module.exports = function (app) {
+  // Load index page
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+  });
+
+  // Load contact page
+  app.get("/contact", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/contact.html"));
+  });
+
+  // Load volunteer page 
+  app.get("/volunteer", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+  });
+
+  // Load login page
+  app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/authTest.html"));
+  });
+};
